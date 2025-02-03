ad855335261cd75ca4e69c17d3051b195388b6d5
Silas Stilling-Jørgensen <sis002@edu.zealand.dk>
2025-02-03 13:34:37 +0100

    Add project files.

---
+++ WebShopLibrary/Product.cs
@@ -0,0 +1,54 @@
+﻿namespace WebShopLibrary
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Model { get; set; }
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Product: {Name}, Price: {Price}";
+        }
+
+        public void ValidateName()
+        {
+            if (Name == null)
+            {
+                throw new ArgumentNullException(nameof(Name), "Name cannot be null");
+            }
+            if (Name.Length <= 2)
+            {
+                throw new ArgumentOutOfRangeException("Name must be at least 2 characters long", nameof(Name));
+            }
+
+        }
+
+        public void ValidateModel()
+        {
+            if (Model == null)
+            {
+                throw new ArgumentNullException(nameof(Model), "Model cannot be null");
+            }
+            if (Model.Length <= 2)
+            {
+                throw new ArgumentOutOfRangeException("Model must be at least 2 characters long", nameof(Model));
+            }
+        }
+        public void ValidatePrice()
+        {
+            if (Price <= 148)
+            {
+                throw new ArgumentOutOfRangeException("Price must be greater than 148", nameof(Price));
+            }
+        }
+        public void Validate()
+        {
+            ValidateName();
+            ValidateModel();
+            ValidatePrice();
+        }
+
+    }
+}
